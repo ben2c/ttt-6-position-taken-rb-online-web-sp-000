6de376da86566bf0ba5a61b80b6258f31bdf903a
Benjamin Cheng <bencheng200@gmail.com>
2020-11-08 15:34:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,14 @@
 def position_taken? (board, index)
   if board[index] == " "
     false
-  end
-  if board[index] == ""
+  elsif board[index] == ""
     false
+  elsif board[index] == nil
+      false
+
+  elsif board[index] == "X" || board[index] == "O"
+      false
+
   end
 
 end
